49067764f121c4226672ba33a4cdc862dc91dbca
jiyeon7269 <jiyeon7269@naver.com>
2017-12-23 04:29:50 +0900

    박보검박보검

--- Condi_Android/app/src/main/java/com/example/food8/condi_android/MainActivity.java
+++ Condi_Android/app/src/main/java/com/example/food8/condi_android/MainActivity.java
@@ -7,22 +7,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-<<<<<<< HEAD
-    Retrofit retrofit;
-    ApiService apiService;
-=======
->>>>>>> 4b2eb4f3c91712c2d8d433a6e3333f0b22b57fae
+
     Button btn_yes,btn_no;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-        retrofit = new Retrofit.Builder().baseUrl(ApiService.API_URL).build();
-        apiService = retrofit.create(ApiService.class);
-=======
->>>>>>> 4b2eb4f3c91712c2d8d433a6e3333f0b22b57fae
+
         btn_yes = (Button)findViewById(R.id.btn_yes);
         btn_no = (Button)findViewById(R.id.btn_no);
 
