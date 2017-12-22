425f2988358c3eb89cf3ee997de07e0271993097
food869 <food869@naver.com>
2017-12-23 06:54:43 +0900

    ㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁㅁ

--- Condi_Android/app/src/main/java/com/example/food8/condi_android/ContactActivity.java
+++ Condi_Android/app/src/main/java/com/example/food8/condi_android/ContactActivity.java
@@ -1,13 +1,75 @@
 package com.example.food8.condi_android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
 
 public class ContactActivity extends AppCompatActivity {
+    Intent intent;
+    boolean[] array;
+    String sendData = "asdf";
+    Retrofit retrofit;
+    ApiService apiService;
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        intent = getIntent();
+        array = getIntent().getBooleanArrayExtra("array");
+        textView = (TextView) findViewById(R.id.resultText);
+        retrofit = new Retrofit.Builder().baseUrl(ApiService.API_URL).build();
+        button = (Button)findViewById(R.id.btnget);
+        apiService = retrofit.create(ApiService.class);
+        if(array[0]&&!array[1]&&!array[2]){     //1번 참
+            sendData = "delighted";
+        }else if(!array[0]&&array[1]&&!array[2]){   //2번참
+            sendData = "happy";
+        }else if(!array[0]&&!array[1]&&array[2]){   //3번 참
+            sendData = "nervous";
+        }else if(array[0]&&array[1]&&!array[2]){    //1,2 참
+            sendData = "gloomy";
+        }else if(array[0]&&!array[1]&&array[2]){    //1,3 참
+            sendData = "serious";
+        }else if(!array[0]&&array[1]&&array[2]){    //2,3 참
+            sendData = "love";
+        }
+        textView.setText(sendData);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<ResponseBody> data = apiService.getData(sendData);
+                data.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        try {
+                            Log.v("Test",response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+
     }
 }
--- Condi_Android/app/src/main/java/com/example/food8/condi_android/SelectActivity.java
+++ Condi_Android/app/src/main/java/com/example/food8/condi_android/SelectActivity.java
@@ -1,25 +1,79 @@
 package com.example.food8.condi_android;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class SelectActivity extends AppCompatActivity {
-    Button btn_select;
+
+    RadioGroup answer1,answer2,answer3;
+    LinearLayout content1,content2,content3;
+    Button submitBtn;
+    boolean[] array = new boolean[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+        answer1 = (RadioGroup) findViewById(R.id.answer_1);
+        answer2 = (RadioGroup) findViewById(R.id.answer_2);
+        answer3 = (RadioGroup) findViewById(R.id.answer_3);
 
-        btn_select = (Button)findViewById(R.id.btn_select);
+        content1 = (LinearLayout) findViewById(R.id.linearLayout);
+        content2 = (LinearLayout) findViewById(R.id.linearLayout2);
+        content3 = (LinearLayout) findViewById(R.id.linearLayout3);
 
-        btn_select.setOnClickListener(new View.OnClickListener() {
+        submitBtn = (Button) findViewById(R.id.submitBtn);
+
+        answer1.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                content2.setVisibility(View.VISIBLE);
+            }
+        });
+
+        answer2.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                content3.setVisibility(View.VISIBLE);
+            }
+        });
+
+        answer3.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+            }
+        });
+
+        submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                RadioButton rd1 = (RadioButton) findViewById(answer1.getCheckedRadioButtonId());
+                RadioButton rd2 = (RadioButton) findViewById(answer2.getCheckedRadioButtonId());
+                RadioButton rd3 = (RadioButton) findViewById(answer3.getCheckedRadioButtonId());
+                if(rd1.getId()==R.id.radio1_yes){
+                    array[0] = true;
+                }else if(rd1.getId()==R.id.radio1_no){
+                    array[0] = false;
+                }
+                if(rd2.getId()==R.id.radio2_yes){
+                    array[1] = true;
+                }else if(rd2.getId()==R.id.radio2_no){
+                    array[1] = false;
+                }
+                if(rd3.getId()==R.id.radio3_yes){
+                    array[2] = true;
+                }else if(rd3.getId()==R.id.radio3_no){
+                    array[2] = false;
+                }
                 Intent intent = new Intent(getApplicationContext(),ContactActivity.class);
+                intent.putExtra("array",array);
                 startActivity(intent);
             }
         });
