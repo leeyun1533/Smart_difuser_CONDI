88d5ee62b0da3b4a881876631280e24d45540d6f
jiyeon7269 <jiyeon7269@naver.com>
2017-12-23 01:33:14 +0900

    로고 추가

--- Condi_Android/app/src/main/java/com/example/food8/condi_android/SelectActivity.java
+++ Condi_Android/app/src/main/java/com/example/food8/condi_android/SelectActivity.java
@@ -1,13 +1,27 @@
 package com.example.food8.condi_android;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SelectActivity extends AppCompatActivity {
+    Button btn_select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select);
+
+        btn_select = (Button)findViewById(R.id.btn_select);
+
+        btn_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),ContactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
