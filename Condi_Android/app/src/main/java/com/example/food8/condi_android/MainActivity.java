cc1eb6a2760233b053dfda429f499de457bc492b
food869 <food869@naver.com>
2017-12-22 17:25:14 +0900

    Condi Android retrofit get Data 구현

---
+++ Condi_Android/app/src/main/java/com/example/food8/condi_android/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.food8.condi_android;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.IOException;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+public class MainActivity extends AppCompatActivity {
+    Retrofit retrofit;
+    ApiService apiService;
+    Button button;
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        retrofit = new Retrofit.Builder().baseUrl(ApiService.API_URL).build();
+        apiService = retrofit.create(ApiService.class);
+        button = (Button)findViewById(R.id.button3);
+        editText = (EditText)findViewById(R.id.editText);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<ResponseBody> data = apiService.getData(editText.getText().toString());
+                data.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        try {
+                            Log.v("Test",response.body().string());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable t) {
+
+                    }
+                });
+            }
+        });
+    }
+}
